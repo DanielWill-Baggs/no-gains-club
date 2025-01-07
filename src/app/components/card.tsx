d849de89e6dd2214b2c5f9c8f6860c5a78f3e3b3
Daniel Baggs <danielwillbaggs@gmail.com>
2025-01-07 18:39:08 -0500

    bug: added necessary structure to card tsx file

--- src/app/components/card.tsx
+++ src/app/components/card.tsx
@@ -1,8 +1,25 @@
 import styles from "./../styles/card.module.css";
-export default function card() {
+import Image from "next/image";
+
+export default function card(...cardProps: any) {
   return (
     <div className={styles.container}>
       <div className={styles.card}>
+        <div className={styles.cardImageContainer}>
+          <div className={styles.cardImage}>
+            <Image src="" alt="{}" width={310} height={313} />
+          </div>
+          <svg viewBox="0 0 310 313" style={{ width: "100%", height: "100%" }}>
+            <rect
+              className={styles.cardImageRectangle}
+              x="0"
+              y="0"
+              width="310"
+              height="313"
+              fill="#F2F2F2"
+            />
+          </svg>
+        </div>
         <h2>Card</h2>
       </div>
     </div>
