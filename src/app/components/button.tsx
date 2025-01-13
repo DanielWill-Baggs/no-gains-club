24e8c022b8112b2aeac93e5fd6ac8a8a21fe8a18
Daniel Baggs <danielwillbaggs@gmail.com>
2025-01-13 16:43:37 -0500

    chore: updated button component to take inline styling properties

--- src/app/components/button.tsx
+++ src/app/components/button.tsx
@@ -1,6 +1,24 @@
 import React from "react";
 import styles from "../styles/button.module.css";
 
-export const GenericButton = ({ text }: { text: string }) => (
-  <button className={styles.genericButton}>{text}</button>
+interface GenericButtonProps {
+  text: string;
+  className?: string;
+  inlineStyle?: React.CSSProperties;
+  onClick?: () => void;
+}
+
+export const GenericButton: React.FC<GenericButtonProps> = ({
+  text,
+  className = "",
+  inlineStyle = {},
+  onClick,
+}) => (
+  <button
+    className={`${styles.genericButton} ${className}`}
+    style={inlineStyle}
+    onClick={onClick}
+  >
+    {text}
+  </button>
 );
